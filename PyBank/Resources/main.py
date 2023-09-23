cdd65c3c4df6e279498ec01a3d41d94497cab634
ldeblock1 <lucasdeblock@gmail.com>
2023-09-22 23:19:41 -0400

    python-challenge

---
+++ PyBank/Resources/main.py
@@ -0,0 +1,64 @@
+import os
+import csv
+
+budget_data = []
+
+csv_path = os.path.join("Resources","budget_data.csv")
+#open csv file
+with open(csv_path) as csvfile:
+    budget_data_read = csv.reader(csvfile, delimiter=",")
+
+    next(budget_data_read)
+
+#read budget data into table
+    
+    for row in budget_data_read:
+        budget_data.append(row)
+
+#Get the month_count
+    month_count = len(budget_data)
+
+#Find the total by looping through the rows of column 2
+    total = 0
+    for rows in budget_data:
+        total += int(rows[1])
+#initialize variables
+    Totaldifference = 0
+    #the number of differences in the data set is -1 bc it takes two numbers at a time to subtract
+    num_differences = month_count-1
+    greatest_increase = 0
+    greatest_decrease = 0
+
+    for rows2 in range(num_differences):
+        #calculate the differences between a row and the previous row
+        diff = int(budget_data[rows2+1][1]) - int(budget_data[rows2][1]) 
+        #add them up
+        Totaldifference += diff
+        #If the diff found is greater than the current greatest increase replace the current & retrieve the corresponding date. 
+        if diff > greatest_increase or greatest_increase == 0:  
+            greatest_increase = diff
+            greatest_increase_date = budget_data[rows2 + 1][0]
+        #Same for greatest decrease Capture it and store it 
+        if diff < greatest_decrease or greatest_decrease == 0:  
+            greatest_decrease = diff
+            greatest_decrease_date = budget_data[rows2 + 1][0]
+    #calculate average change which equals the total differences/the total number of differences it found (number of times it subtracted) 
+    average_change = Totaldifference/num_differences
+
+output_data = (
+      f"\nFinancial Analysis\n\n"
+      f"--------------------------\n\n"
+      f"Total number of months: {month_count}\n\n" 
+      f"Total: {total}\n\n"
+      f"Average Change: $ {round(average_change,2)}\n\n"
+      f"Greatest Increase in Profits: {greatest_increase_date} ($+{greatest_increase})\n\n"
+      f"Greatest Decrease in Profits: {greatest_decrease_date}  (${greatest_decrease})\n\n"
+      )
+print(output_data)
+#export output data in a text file
+output_path = os.path.join("Resources", "output.txt")
+with open(output_path, 'w') as file:
+    file.write(output_data)
+
+
+    
---
+++ PyPoll/Resources/main.py
@@ -0,0 +1,58 @@
+import os
+import csv
+
+election_data = []
+csv_path = os.path.join("Resources","election_data.csv")
+
+with open(csv_path) as csvfile:
+    election_data_read = csv.reader(csvfile, delimiter=",")
+    next(election_data_read)
+
+    for row in election_data_read:
+        election_data.append(row)
+
+#Total number of votes cast
+    total_votes = len(election_data)
+
+#The Candidates and their votes recieved
+    Candidate_Dict = {}
+    Candidate_count = 0
+    for rows in election_data:
+
+        Current_Candidate = rows[2]
+        
+        if Current_Candidate in Candidate_Dict:
+            Candidate_Dict[Current_Candidate] += 1
+        else:
+            Candidate_Dict[Current_Candidate] = 1
+
+
+    Candidates = list(Candidate_Dict)
+    results = ""
+    Winner = ""
+    max_votes = 0
+    for candidate in Candidates:
+        percent_share = (Candidate_Dict[candidate]/total_votes) * 100
+        results += (f"{candidate}: {Candidate_Dict[candidate]} ({percent_share}%)\n\n" )
+
+        if Candidate_Dict[candidate] > max_votes:
+            max_votes = Candidate_Dict[candidate]
+            Winner = candidate
+
+
+output = (
+    f"\nElection Results\n\n"
+    f"--------------------------------\n\n"
+    f"Total Votes Cast: {total_votes}\n\n"
+    f"--------------------------------\n\n"
+    f"{results}"
+    f"--------------------------------\n\n"
+    f"Winner: {Winner} with votes: {max_votes}\n\n"
+    f"--------------------------------\n"
+    )
+
+print(output)
+
+output_path = os.path.join("Resources", "output.txt")
+with open(output_path, 'w') as file:
+    file.write(output)
